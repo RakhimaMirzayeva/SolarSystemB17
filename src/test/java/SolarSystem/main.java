1848345a6ae864997b00ded8b36e42945ed35ad1
rakhimamirzayeva <rakhimamirzayeva91@gmail.com>
2023-11-01 19:52:57 -0400

    this commiy has the feature sun added

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -1,13 +1,19 @@
 package SolarSystem;
 
 public class main {
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
 
 
-        solarSystemBasics solarsys = new solarSystemBasics();
-        solarsys.Sun = "1";
-        solarsys.Moon = "2";
-        solarsys.Planets = "2";
-        solarsys.Stars = "10";
+        solarSystemBasics solarSys = new solarSystemBasics();
+        solarSys.Sun = "1";
+        solarSys.Moon = "2";
+        solarSys.Planets = "2";
+        solarSys.Stars = "10";
+
+        //        **Feature Sun**
+        FeatureSun Sun=new FeatureSun();
+        Sun.name="panther";
+        Sun.color="black";
+        Sun.temp="-1223124kelvin";
     }
 }
